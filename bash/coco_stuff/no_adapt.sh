3f68e2729fed1efabf4cfa876372dcae692c87b1
meno <me.noori.1994@gmail.com>
2025-05-27 00:13:11 -0400

    Add bash files

---
+++ bash/coco_stuff/no_adapt.sh
@@ -0,0 +1,46 @@
+# GPU Configuration
+GPU_ID=0
+
+# Dataset Configuration
+DATASET=COCOStuffDataset
+DATA_DIR=".data/coco_stuff164k/"
+INIT_RESIZE="224 224"
+ALL_CORRUPTIONS="original"
+WORKERS=4
+
+# Method and OVSS Model Configuration
+OVSS_TYPE="naclip"
+OVSS_BACKBONE="ViT-L/14"
+
+# Hyperparameters
+BATCH_SIZE=2
+LR=0.001
+STEPS=10
+TRIALS=3
+
+# Output
+SAVE_DIR=".save/${DATASET}/No_Adaptation/"
+
+# Run
+CUDA_VISIBLE_DEVICES=$GPU_ID python main.py \
+                        --ovss_type $OVSS_TYPE \
+                        --ovss_backbone $OVSS_BACKBONE \
+                        \
+                        --save_dir $SAVE_DIR \
+                        --data_dir $DATA_DIR \
+                        --dataset $DATASET \
+                        --workers $WORKERS \
+                        --init_resize $INIT_RESIZE \
+                        --patch_size 224 224 \
+                        --patch_stride 112 \
+                        --corruptions_list $ALL_CORRUPTIONS \
+                        \
+                        --lr $LR \
+                        --steps $STEPS \
+                        --batch-size $BATCH_SIZE \
+                        --trials $TRIALS \
+                        --seed 0 \
+                        \
+                        --plot_loss \
+                        --class_extensions
+
